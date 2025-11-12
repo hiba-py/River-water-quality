d64a34eef36eb87a03bd8fbe5aebb1024fac85e8
hme52 <hme52@uclive.ac.nz/opt/miniconda3/envs/COSC480/bin/python>
2025-11-12 14:50:40 +1300

    Add input validation and no-records warnings
    Previously, program would produce error when user entered invalid year or river names
    
    - Validate year and river name inputs against available data
    - Helpful error message appears when invalid data is entered
    - Prompt user to re-enter input until valid
    - Add two helper functions to handle validation and data extraction
    
    Outcome: Improves user experience and prevents crashes

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def menu_select(options: list[str]) -> int:
     - The user is prompted until they enter a valid menu index
     - returns valid user selection
     """
-    prompt = f"0-{len(options) - 1}:: "
+    prompt = f"0-{len(options) - 1}: "
     i = 0
     while i < len(options):
         print(f'[{i}] {options[i]}')
@@ -58,6 +58,26 @@ def print_water_quality_report(year_of_interest: int, river_names: list[str]) ->
                     max_val = reading
         print(f"{name:15}{total /  count:^10.2f}{count:^15}{min_val:^8}{max_val:^8}")
 
+def extract_valid_year_and_river():
+    """ Reads data input and returns range for year and valid river names """
+    data = read_csv_data(DATA_FILE, ["river", "sDate", "values"])
+    years = []
+    all_rivers = []
+
+    for i in range(len(data)):
+        year = datetime.strptime(data[i][1], "%d/%m/%Y").year
+        if year not in years:
+            years.append(year)
+        if data[i][0] not in all_rivers:
+            all_rivers.append(data[i][0])
+    return (min(years), max(years)), all_rivers
+
+def validate_river(all_rivers, user_input):
+    """ Checks each user inputted river name in data. Returns True if all names are in data """
+    for i in range(len(user_input)):
+        if user_input[i].strip() not in all_rivers:
+            return False
+    return True
 
 def main():
     """Small application that presents tables and graphs based on water quality data."""
@@ -66,11 +86,19 @@ def main():
         "Water Quality Over Time Graph",
         "Exit"
     ]
+    years, rivers = extract_valid_year_and_river()
     option = menu_select(menu_options)
     if option == 0:
         year = int(input("Year: "))
+        while year not in range(years[0], years[1] + 1):
+            print(f'Sorry, no data available for the year {year}. Please enter a year between {years[0]} and {years[1]}')
+            year = int(input('Year: '))
         rivers_string = input("River Names: ")
         river_names = rivers_string.split(",")
+        while not validate_river(rivers, river_names):
+            print(f'Sorry, no data available for {', '.join([str(river) for river in river_names])}. Please enter another river')
+            rivers_string = input("River Names: ")
+            river_names = rivers_string.split(",")
         print_water_quality_report(year, river_names)
     elif option == 1:
         print("Not Implemented Yet")
